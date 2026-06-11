900fb41891057c18d8b329d930d691e233df239d
Elricc816 <vibhordhekwar944@gmail.com>
2026-06-11 15:17:09 +0530

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,12 +1,13 @@
 module.exports = {
   name: "ban",
-  execute(message, args) {
+  async execute(message, args) {
 
     if (!message.member.permissions.has("BanMembers")) {
       return message.reply("❌ You don't have permission to ban members!");
     }
 
     const user = message.mentions.members.first();
+
     if (!user) {
       return message.reply("❌ Please mention a user to ban!");
     }
@@ -19,8 +20,30 @@ module.exports = {
       return message.reply("❌ I cannot ban this user!");
     }
 
-    user.ban();
+    // optional reason
+    const reason = args.slice(1).join(" ");
 
-    message.reply(`🔨 Successfully banned **${user.user.tag}**`);
+    let dmFailed = false;
+
+    // DM user before ban
+    try {
+      await user.send(
+        reason
+          ? `🔨 You have been banned from **${message.guild.name}** server\nReason: ${reason}`
+          : `🔨 You have been banned from **${message.guild.name}** server`
+      );
+    } catch (err) {
+      dmFailed = true;
+    }
+
+    // ban user
+    await user.ban({ reason: reason || "No reason provided" });
+
+    // server reply
+    message.reply(
+      `🔨 Successfully banned **${user.user.tag}**` +
+      (reason ? `\nReason: ${reason}` : "") +
+      (dmFailed ? "\n⚠️ Could not DM user" : "")
+    );
   }
 };
