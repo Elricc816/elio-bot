a2f0283ab49b92eefc3a8a93004a79066e827752
Elricc816 <vibhordhekwar944@gmail.com>
2026-06-12 10:38:57 +0530

    Update ping.js

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,25 +1,73 @@
-const { EmbedBuilder } = require('discord.js');
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require('discord.js');
 
 module.exports = {
   name: "ping",
   async execute(message) {
 
-    const heartbeat = Date.now() - message.createdTimestamp;
-    const api = Math.round(message.client.ws.ping);
+    const createEmbed = () => {
+      const heartbeat = Date.now() - message.createdTimestamp;
+      const api = Math.round(message.client.ws.ping);
 
-    const embed = new EmbedBuilder()
-      .setAuthor({
-        name: message.client.user.username,
-        iconURL: message.client.user.displayAvatarURL()
-      })
-      .setDescription(
-        `<:heartt:1514699719400755432> Heartbeat: **${heartbeat} ms**\n<:timerr:1514699712681218094> API: **${api} ms**`
-      )
-      .setColor('#fff18d')
-      .setFooter({
-        text: 'Cluster: 0 | Shard: 0'
+      return new EmbedBuilder()
+        .setAuthor({
+          name: message.client.user.username,
+          iconURL: message.client.user.displayAvatarURL()
+        })
+        .setDescription(
+          `<:heartt:1514699719400755432> Heartbeat: **${heartbeat} ms**\n<:timerr:1514699712681218094> API: **${api} ms**`
+        )
+        .setColor('#fff18d')
+        .setFooter({
+          text: 'Cluster: 0 | Shard: 0'
+        });
+    };
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId('refresh_Ping')
+        .setLabel('Refresh')
+        .setEmoji('<:pin:1514699935264673902>')
+        .setStyle(ButtonStyle.Secondary)
+    );
+
+    const msg = await message.reply({
+      embeds: [createEmbed()],
+      components: [row]
+    });
+
+    const collector = msg.createMessageComponentCollector({
+      time: 300000 // 5 min
+    });
+
+    collector.on('collect', async interaction => {
+
+      if (interaction.customId !== 'refresh_ping') return;
+
+      const heartbeat = Date.now() - interaction.createdTimestamp;
+      const api = Math.round(message.client.ws.ping);
+
+      const updatedEmbed = new EmbedBuilder()
+        .setAuthor({
+          name: message.client.user.username,
+          iconURL: message.client.user.displayAvatarURL()
+        })
+        .setDescription(
+          `<:heartt:1514699719400755432> Heartbeat: **${heartbeat} ms**\n<:timerr:1514699712681218094> API: **${api} ms**`
+        )
+        .setColor('#fff18d')
+        .setFooter({
+          text: 'Cluster: 0 | Shard: 0'
+        });
+
+      await interaction.update({
+        embeds: [updatedEmbed],
+        components: [row]
       });
-
-    message.reply({ embeds: [embed] });
+    });
   }
 };
