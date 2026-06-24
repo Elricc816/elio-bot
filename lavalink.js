81ad2929c1fb19fd7e44cd723ce0bffb891c0cb7
Elricc816 <vibhordhekwar944@gmail.com>
2026-06-24 19:56:50 +0530

    Create lavalink.js

---
+++ lavalink.js
@@ -0,0 +1,12 @@
+const { Connectors } = require("shoukaku");
+
+const nodes = [
+  {
+    name: "main",
+    url: "https://lavalink-latest-bcvc.onrender.com:2333",
+    auth: "youshallnotpass",
+    secure: false
+  }
+];
+
+module.exports = { nodes };
