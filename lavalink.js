b0da475d417d6a14c988b2b9430bffe9f591b4c7
Elricc816 <vibhordhekwar944@gmail.com>
2026-06-25 12:54:45 +0530

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,10 +1,7 @@
-const { Connectors } = require("shoukaku");
-
-// Lavalink nodes (Render / hosted Lavalink)
 const nodes = [
   {
     name: "main",
-    url: "ws://lavalink-latest-bcvc.onrender.com:2333",
+    url: "lavalink-latest-bcvc.onrender.com:2333",
     auth: "youshallnotpass",
     secure: false
   }
