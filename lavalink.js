b21d4dc05659a6b2f01fd8bda8406e803ca61aba
Elricc816 <vibhordhekwar944@gmail.com>
2026-06-25 15:07:16 +0530

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,7 +1,7 @@
 const nodes = [
   {
     name: "main",
-    url: "lavalink-latest-bcvc.onrender.com:2333",
+    url: "lavalink-latest-bcvc.onrender.com",
     auth: "youshallnotpass",
     secure: false
   }
