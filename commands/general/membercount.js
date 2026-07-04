d1c8cbde5b464dd5157ab4adcf2f6181ca8f17c0
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-07-05 00:23:44 +0530

    Update membercount.js

--- commands/general/membercount.js
+++ commands/general/membercount.js
@@ -18,16 +18,16 @@ module.exports = {
         const offline = members.filter(m => !m.presence || m.presence.status === "offline").size;
 
         const embed = new EmbedBuilder()
-            .setColor("#D3D3D3")
-            .setTitle("<:member1:1514699741282304061> Member Count")
-            .setDescription(
+    .setColor("#D3D3D3")
+    .setTitle("<:member1:1514699741282304061> Member Count")
+    .setThumbnail(message.guild.iconURL({ dynamic: true, size: 1024 }))
+    .setDescription(
 `> **__Humans__ :** \`${humans}\`
 > **__Bots__ :** \`${bots}\`
 > **__Total__ :** \`${members.size}\`
 <:dot:1514706694079254730> **Online :** \`${online}\`
 <:dot:1514706694079254730> **Offline :** \`${offline}\``
-                .setThumbnail(message.guild.iconURL({ dynamic: true, size: 1024 }))
-            );
+    );
 
         return message.reply({ embeds: [embed] });
     }
