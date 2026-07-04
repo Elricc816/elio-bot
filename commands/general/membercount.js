81176cac0acac0b8c7607887098b002e5d08776c
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-07-05 00:15:58 +0530

    Update membercount.js

--- commands/general/membercount.js
+++ commands/general/membercount.js
@@ -3,11 +3,13 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     name: "membercount",
 
-    async execute(message, args, client) {
+    async execute(message) {
 
         const guild = message.guild;
 
-        const members = await guild.members.fetch();
+        await guild.members.fetch().catch(() => null);
+
+        const members = guild.members.cache;
 
         const humans = members.filter(m => !m.user.bot).size;
         const bots = members.filter(m => m.user.bot).size;
@@ -15,15 +17,13 @@ module.exports = {
         const online = members.filter(m => m.presence?.status === "online").size;
         const offline = members.filter(m => !m.presence || m.presence.status === "offline").size;
 
-        const total = guild.memberCount;
-
         const embed = new EmbedBuilder()
             .setColor("#D3D3D3")
             .setTitle("<:member1:1514699741282304061> Member Count")
             .setDescription(
 `> **__Humans__ :** \`${humans}\`
 > **__Bots__ :** \`${bots}\`
-> **__Total__ :** \`${total}\`
+> **__Total__ :** \`${members.size}\`
 <:dot:1514706694079254730> **Online :** \`${online}\`
 <:dot:1514706694079254730> **Offline :** \`${offline}\``
             );
