3ae053f60b06c14aa6dc352ea2b234cb57d31783
Elrιc  .ᐟ <vibhordhekwar944@gmail.com>
2026-07-21 22:40:59 +0530

    Create uptime.js

---
+++ commands/general/uptime.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder } = require("discord.js");
+
+const uptime = process.uptime();
+
+const days = Math.floor(uptime / 86400);
+const hours = Math.floor((uptime % 86400) / 3600);
+const minutes = Math.floor((uptime % 3600) / 60);
+const seconds = Math.floor(uptime % 60);
+
+const embed = new EmbedBuilder()
+    .setColor("#D3D3D3")
+    .setDescription(
+`**Elara's Uptime !**
+
+       <:arrow:1514699753462566953> I have been online for **__${days}d ${hours}h ${minutes}m ${seconds}s__**`
+    );
+
+return message.reply({
+    embeds: [embed]
+});
